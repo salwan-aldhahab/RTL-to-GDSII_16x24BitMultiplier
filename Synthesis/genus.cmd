fae61dc072055a9590828ee0a8068afb7d58de4e
Salwan Aldhahab <salwan99@ruby.eecs.yorku.ca>
2025-11-06 15:36:18 -0500

    edited simulation and synthesis files + running the expirement for mult_unsigned.v

---
+++ Synthesis/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 21.17-s066_1, built Mar 15 2023 09:43:30
+
+# Date: Thu Nov 06 13:32:31 2025
+# Host: ruby (x86_64 w/Linux 4.18.0-553.66.1.el8_10.x86_64) (12cores*48cpus*2physical cpus*Intel(R) Xeon(R) CPU E5-2690 v3 @ 2.60GHz 30720KB)
+# OS:   Rocky Linux release 8.10 (Green Obsidian)
+
+source scripts/genus_script.tcl
